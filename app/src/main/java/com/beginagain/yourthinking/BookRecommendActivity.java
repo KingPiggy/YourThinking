adf41ec0b23af68a1c29a5c7354746550bd161bb
kingpiggylsh <lsoonnoon@gmail.com>
2019-07-02 16:11:11 +0900

    추천도서 조회 스피너 구현

--- app/src/main/java/com/beginagain/yourthinking/BookRecommendActivity.java
+++ app/src/main/java/com/beginagain/yourthinking/BookRecommendActivity.java
@@ -4,21 +4,38 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class BookRecommendActivity extends AppCompatActivity {
+    Spinner mCategorySpinner;
+    TextView mTempTv;
 
-    TextView temp;
-
-    String bookInfo1  = "";
+    int categoryNo[] = {100010, 100020, 100030, 100040, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230
+            , 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_recommend);
 
+        mTempTv = (TextView) findViewById(R.id.temp_recommend);
 
-        Log.d("hoon", "어싱크 실행 전");
+        mCategorySpinner = (Spinner) findViewById(R.id.spinner_recommend_category);
+        mCategorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                mTempTv.setText(item + " : " + categoryNo[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         new JsoupAsyncTask().execute();
 
@@ -33,7 +50,6 @@ public class BookRecommendActivity extends AppCompatActivity {
 
         @Override
         protected Void doInBackground(Void... params) {
-
 
 
             return null;
