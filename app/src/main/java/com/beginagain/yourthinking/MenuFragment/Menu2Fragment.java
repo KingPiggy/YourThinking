9e174dfcd50bbb93146b79c4d5fd7a3ae5bc77bb
kingpiggylsh <lsoonnoon@gmail.com>
2019-07-17 19:05:12 +0900

    7.17 UI 대폭 추가 1

--- app/src/main/java/com/beginagain/yourthinking/MenuFragment/Menu2Fragment.java
+++ app/src/main/java/com/beginagain/yourthinking/MenuFragment/Menu2Fragment.java
@@ -1,27 +1,62 @@
 package com.beginagain.yourthinking.MenuFragment;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.beginagain.yourthinking.MakeChatRoomInfoActivity;
 import com.beginagain.yourthinking.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class Menu2Fragment extends Fragment {
     View view;
+
+    Button mRoomMakeBtn, mRoomSearchBtn;
+    ListView mChatRoomListView;
+
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> room = new ArrayList<>();
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference().getRoot();
+    private String name;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_menu2, container, false);
 
+        init();
 
+        arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, room);
+        mChatRoomListView.setAdapter(arrayAdapter);
 
-
+        mRoomMakeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), MakeChatRoomInfoActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
+    }
+
+    void init() {
+        mRoomMakeBtn = (Button) view.findViewById(R.id.btn_make_chat_makeroom);
+        mRoomSearchBtn = (Button) view.findViewById(R.id.btn_make_chat_searchroom);
+        mChatRoomListView = (ListView) view.findViewById(R.id.listview_make_chat_chatlist);
     }
 }
--- app/src/main/java/com/beginagain/yourthinking/MenuFragment/Menu5Fragment.java
+++ app/src/main/java/com/beginagain/yourthinking/MenuFragment/Menu5Fragment.java
@@ -1,30 +1,43 @@
 package com.beginagain.yourthinking.MenuFragment;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.beginagain.yourthinking.R;
 import com.beginagain.yourthinking.UserInfoActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Menu5Fragment extends Fragment {
     View view;
-    Button mUserInfoBtn;
+    ViewGroup profileLayout;
+    TextView mUserName, mUserEmail;
+    de.hdodenhof.circleimageview.CircleImageView mUserImage;
+
+    String name, email;
+    Uri photoUrl;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_menu5, container, false);
+        init();
 
-        mUserInfoBtn = (Button) view.findViewById(R.id.btn_menu5_userinfo);
-        mUserInfoBtn.setOnClickListener(new View.OnClickListener() {
+        profileLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), UserInfoActivity.class);
@@ -32,7 +45,25 @@ public class Menu5Fragment extends Fragment {
             }
         });
 
-
         return view;
+    }
+
+    void init() {
+        profileLayout = (ViewGroup) view.findViewById(R.id.layout_menu5_profile);
+        mUserName = (TextView) view.findViewById(R.id.text_view_menu5_profile_name);
+        mUserEmail = (TextView) view.findViewById(R.id.text_view_menu5_profile_email);
+        mUserImage = (CircleImageView) view.findViewById(R.id.image_menu5_profile_image);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            name = user.getDisplayName();
+            email = user.getEmail();
+            photoUrl = user.getPhotoUrl();
+
+            mUserName.setText(name);
+            mUserEmail.setText(email);
+            Picasso.get().load(photoUrl).into(mUserImage);
+        }
     }
 }
